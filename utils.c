1f36f10364ed152a819443f9e07e8f78fafca421
John Lindgren <john@jlindgren.net>
2018-05-22 22:00:30 -0400

    Add copyright headers.

--- utils.c
+++ utils.c
@@ -1,6 +1,21 @@
-/* J Login >> utils.c */
-/* John Lindgren */
-/* May 26, 2012 */
+/*
+ * J-Login - utils.c
+ * Copyright 2010-2015 John Lindgren
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions, and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions, and the following disclaimer in the documentation
+ *    provided with the distribution.
+ *
+ * This software is provided "as is" and without any warranty, express or
+ * implied. In no event shall the authors be liable for any damages arising from
+ * the use of this software.
+ */
 
 #define _DEFAULT_SOURCE
 #define _XOPEN_SOURCE
